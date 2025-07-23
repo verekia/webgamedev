24081c7cddfebdc0679192eef9fe01e5e527413e
verekia <522007+verekia@users.noreply.github.com>
2025-07-23 16:15:41 +0700

    content: blender page

--- src/pages/engines-libraries/_meta.ts
+++ src/pages/engines-libraries/_meta.ts
@@ -3,6 +3,7 @@ const meta = {
   poimandres: { title: '⚫️ Poimandres' },
   react: { title: '⚛️ React' },
   'dev-tools': { title: '🧰 Dev Tools' },
+  'blender': { title: '🐵 Blender' },
   'native-game-engines': { title: '🎮 Native Engines' },
   'webxr-vr-ar': { title: '🥽 WebXR (VR, AR)' },
   'rust-game-dev': { title: '🦀 Rust Game Dev' },
