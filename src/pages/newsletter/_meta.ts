fe0e390e0b8ddeccaa4f36699021b8aebde73b4f
verekia <522007+verekia@users.noreply.github.com>
2026-06-25 20:55:35 +0700

    content: 031 wip

--- src/pages/newsletter/_meta.ts
+++ src/pages/newsletter/_meta.ts
@@ -1,6 +1,6 @@
 const meta = {
   template: { title: 'Issue XXX – New! 🎉', display: 'hidden' },
-  '031': { title: 'Issue 031 – New! 🎉', display: 'hidden' },
+  '031': { title: 'Issue 031 – New! 🎉' },
   '030': { title: 'Issue 030' },
   '029': { title: 'Issue 029' },
   '028': { title: 'Issue 028' },
